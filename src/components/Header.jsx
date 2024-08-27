bc348f3eef062e5e9e98067be094d90c57e094f3
Kishan Kumar <singhkishan9911@gmail.com>
2024-08-27 15:48:53 +0530

    add back button in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { RiMenu3Line } from 'react-icons/ri'
 import { useDispatch, useSelector } from 'react-redux'
 import logo from '../assets/logo.png'
+import { IoArrowBackOutline } from 'react-icons/io5'
+import { useNavigate } from 'react-router-dom'
 
-function Header({title}) {
+function Header({ title }) {
   const { userData } = useSelector((state) => state.user)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   return (
     <header className='w-full h-fit flex gap-x-2 py-1'>
@@ -15,12 +18,13 @@ function Header({title}) {
       })}>
         <RiMenu3Line className=' text-2xl font-semibold' />
       </button>
-      <div className='w-full flex items-center justify-between'>
-      <h3 className='text-xl md:text-3xl font-medium '>{title}</h3>
-      <div className=' flex flex-col items-end gap-2'>
-        <img src={logo} alt="" className=' w-32' />
-        {userData && <p>Welcome, {userData?.name}</p>}
-      </div>
+      <div className='w-full flex gap-2 items-center justify-start'>
+        <button onClick={() => navigate(-1)} className='p-[10px] rounded-full hover:bg-gray-100 hidden md:block'><IoArrowBackOutline /></button>
+        <h3 className='text-xl md:text-3xl font-medium '>{title}</h3>
+        <div className='ms-auto flex flex-col items-end gap-2'>
+          <img src={logo} alt="" className=' w-32' />
+          {userData && <p>Welcome, {userData?.name}</p>}
+        </div>
       </div>
     </header>
   )
